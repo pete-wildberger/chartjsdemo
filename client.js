60f7029e2e4960e4dccf2322813b66874ae4b5fc
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-23 15:08:43 -0500

    line graph

--- client.js
+++ client.js
@@ -1,42 +1,96 @@
 console.log('js is sourced');
 var canvas = document.getElementById("myChart");
 var ctx = canvas.getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "badass"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3, 6],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(186, 212, 85, 0.2)'
+var chartArr = [];
+var nameArr =[];
 
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                '#bad455'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+for (let i = 0; i < DATA_4_CHART.length; i++) {
+    name = DATA_4_CHART[i].name.first + ' ' + DATA_4_CHART[i].name.last;
+    nameArr.push(name);
+  chartArr.push(DATA_4_CHART[i].age);
+}
+for (let i = 0; i < DATA_4_CHART.length; i++) {
+
+}
+//line
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+            labels: nameArr,
+            datasets: [{
+                label: 'age',
+                data: chartArr,
+        //         backgroundColor: [
+        //             'rgba(255, 99, 132, 0.2)',
+        //             'rgba(54, 162, 235, 0.2)',
+        //             'rgba(255, 206, 86, 0.2)',
+        //             'rgba(75, 192, 192, 0.2)',
+        //             'rgba(153, 102, 255, 0.2)',
+        //             'rgba(255, 159, 64, 0.2)',
+        //             'rgba(186, 212, 85, 0.2)'
+        //
+        //         ],
+        //         borderColor: [
+        //             'rgba(255,99,132,1)',
+        //             'rgba(54, 162, 235, 1)',
+        //             'rgba(255, 206, 86, 1)',
+        //             'rgba(75, 192, 192, 1)',
+        //             'rgba(153, 102, 255, 1)',
+        //             'rgba(255, 159, 64, 1)',
+        //             '#bad455'
+        //         ],
+        //         borderWidth: 1
             }]
+        },
+    options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
         }
-    }
 });
+
+
+//bar graph
+// var myChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "badass"],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: chartArr,
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)',
+//                 'rgba(186, 212, 85, 0.2)'
+//
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)',
+//                 '#bad455'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
---
+++ data.js
@@ -0,0 +1,344 @@
+const DATA_4_CHART = [
+  {
+    "_id": "5974ff9a674dab583044232f",
+    "index": 0,
+    "guid": "9ea9486c-0c74-462a-835b-c258ddfd87b4",
+    "isActive": false,
+    "balance": "$1,363.60",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "green",
+    "name": {
+      "first": "Kaufman",
+      "last": "Tyson"
+    },
+    "company": "FRENEX",
+    "email": "kaufman.tyson@frenex.me",
+    "phone": "+1 (871) 417-2442",
+    "address": "661 Woodbine Street, Bath, Minnesota, 1004",
+    "about": "Do officia non id culpa est reprehenderit laboris ipsum nostrud nisi amet. Laborum Lorem anim quis Lorem sint nisi enim velit amet anim mollit amet. Est qui tempor laborum est reprehenderit laborum adipisicing et eu dolor consectetur amet tempor. Aliquip ut est in irure nostrud laboris cupidatat veniam. Nulla exercitation do labore amet qui anim qui nulla ut aliquip ea. Tempor enim sint excepteur veniam incididunt esse laborum. Magna qui sit esse laboris.",
+    "registered": "Wednesday, February 19, 2014 4:09 AM",
+    "latitude": "-23.982739",
+    "longitude": "115.443573",
+    "tags": [
+      "anim",
+      "occaecat",
+      "ea",
+      "eu",
+      "incididunt"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Weiss Barlow"
+      },
+      {
+        "id": 1,
+        "name": "Mcneil Combs"
+      },
+      {
+        "id": 2,
+        "name": "French Cameron"
+      }
+    ],
+    "greeting": "Hello, Kaufman! You have 7 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5974ff9b996749d775b9aa71",
+    "index": 1,
+    "guid": "f01dca2d-3b9d-41a8-9a34-2120a30bd4b0",
+    "isActive": true,
+    "balance": "$1,278.60",
+    "picture": "http://placehold.it/32x32",
+    "age": 22,
+    "eyeColor": "blue",
+    "name": {
+      "first": "Ann",
+      "last": "Juarez"
+    },
+    "company": "GEEKETRON",
+    "email": "ann.juarez@geeketron.tv",
+    "phone": "+1 (918) 420-3921",
+    "address": "411 Pleasant Place, Westboro, Kansas, 7167",
+    "about": "Sint aute fugiat ad ut mollit proident officia irure esse magna. Ad duis id velit id Lorem elit deserunt minim. Magna velit nulla labore non laborum ut exercitation.",
+    "registered": "Monday, November 16, 2015 6:05 PM",
+    "latitude": "-46.062383",
+    "longitude": "-2.857199",
+    "tags": [
+      "ea",
+      "consectetur",
+      "sit",
+      "veniam",
+      "exercitation"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Wood Cabrera"
+      },
+      {
+        "id": 1,
+        "name": "Norris Nash"
+      },
+      {
+        "id": 2,
+        "name": "Heather Wynn"
+      }
+    ],
+    "greeting": "Hello, Ann! You have 8 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5974ff9b988cea1f4e842e12",
+    "index": 2,
+    "guid": "c197b5e6-b162-40fd-ba39-bf01495bedde",
+    "isActive": false,
+    "balance": "$2,407.79",
+    "picture": "http://placehold.it/32x32",
+    "age": 34,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Cherry",
+      "last": "Moss"
+    },
+    "company": "AQUASSEUR",
+    "email": "cherry.moss@aquasseur.biz",
+    "phone": "+1 (884) 411-2587",
+    "address": "394 Lawn Court, Trona, Delaware, 3798",
+    "about": "Est sit pariatur sint do officia do. Ut dolore sint ullamco cupidatat do id excepteur quis officia ad pariatur Lorem mollit mollit. Duis id cillum quis Lorem ex deserunt esse mollit dolor ipsum et. Incididunt minim reprehenderit eiusmod id irure ut in. Id non ad sint pariatur officia excepteur voluptate. Nostrud incididunt laboris anim elit commodo duis esse. Minim duis adipisicing elit consectetur est sit excepteur commodo aliquip et.",
+    "registered": "Saturday, December 12, 2015 12:37 PM",
+    "latitude": "-15.055424",
+    "longitude": "-129.582935",
+    "tags": [
+      "consequat",
+      "excepteur",
+      "deserunt",
+      "ullamco",
+      "sint"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Adela Villarreal"
+      },
+      {
+        "id": 1,
+        "name": "Barnett King"
+      },
+      {
+        "id": 2,
+        "name": "Hyde Vega"
+      }
+    ],
+    "greeting": "Hello, Cherry! You have 6 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "5974ff9b9f8832ac1d62f588",
+    "index": 3,
+    "guid": "81acfe08-9a9d-4f5f-91a6-a547527fd7a6",
+    "isActive": false,
+    "balance": "$1,108.77",
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Gallagher",
+      "last": "Mccoy"
+    },
+    "company": "BIFLEX",
+    "email": "gallagher.mccoy@biflex.name",
+    "phone": "+1 (814) 461-2210",
+    "address": "410 Commerce Street, Sunnyside, New Jersey, 450",
+    "about": "Irure consequat tempor laborum reprehenderit ullamco. Mollit mollit elit excepteur duis cillum officia proident nostrud adipisicing proident. Ipsum est ut magna ut enim officia quis nostrud esse cillum eu. Qui consectetur proident ea esse amet aute.",
+    "registered": "Friday, April 28, 2017 1:38 AM",
+    "latitude": "-58.804469",
+    "longitude": "130.577579",
+    "tags": [
+      "culpa",
+      "labore",
+      "pariatur",
+      "do",
+      "pariatur"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Rosemarie Le"
+      },
+      {
+        "id": 1,
+        "name": "Robyn Luna"
+      },
+      {
+        "id": 2,
+        "name": "Rosa Schultz"
+      }
+    ],
+    "greeting": "Hello, Gallagher! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5974ff9b5950aaaa2b274a7a",
+    "index": 4,
+    "guid": "4c1ab418-6a9c-4193-b1c5-63c990b0223c",
+    "isActive": false,
+    "balance": "$1,465.31",
+    "picture": "http://placehold.it/32x32",
+    "age": 25,
+    "eyeColor": "blue",
+    "name": {
+      "first": "Jones",
+      "last": "Rodriquez"
+    },
+    "company": "CEMENTION",
+    "email": "jones.rodriquez@cemention.io",
+    "phone": "+1 (922) 512-3799",
+    "address": "338 Aurelia Court, Rivers, West Virginia, 5224",
+    "about": "Occaecat dolor adipisicing velit exercitation tempor ex. Enim consectetur eiusmod aute magna sunt. Ullamco esse sunt aliqua ullamco velit ea eiusmod in Lorem aute ullamco laborum do ipsum. Esse aliqua velit culpa cupidatat aliqua qui commodo cupidatat tempor adipisicing anim laborum. Cupidatat proident nulla irure pariatur ex. Consectetur ullamco eu qui incididunt exercitation cupidatat incididunt officia irure incididunt.",
+    "registered": "Tuesday, November 17, 2015 12:15 PM",
+    "latitude": "63.939513",
+    "longitude": "-40.498978",
+    "tags": [
+      "veniam",
+      "duis",
+      "excepteur",
+      "fugiat",
+      "amet"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Navarro Ware"
+      },
+      {
+        "id": 1,
+        "name": "Woodward Gay"
+      },
+      {
+        "id": 2,
+        "name": "Hickman Morin"
+      }
+    ],
+    "greeting": "Hello, Jones! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "5974ff9b1f126701f13d1402",
+    "index": 5,
+    "guid": "89a5550b-f4b9-422e-99df-c664dfd79ae9",
+    "isActive": false,
+    "balance": "$3,324.08",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "green",
+    "name": {
+      "first": "Ruthie",
+      "last": "Kirby"
+    },
+    "company": "ANDERSHUN",
+    "email": "ruthie.kirby@andershun.co.uk",
+    "phone": "+1 (831) 503-2124",
+    "address": "830 Lawrence Street, Strykersville, Wyoming, 3377",
+    "about": "Occaecat quis veniam sint ullamco. Aliqua minim nostrud cillum non duis esse culpa. Sit sit esse ullamco id laboris commodo minim Lorem. Voluptate officia dolor consequat ad. Anim sit consequat magna elit sit labore commodo laborum cupidatat.",
+    "registered": "Friday, March 17, 2017 6:19 AM",
+    "latitude": "-10.531603",
+    "longitude": "-13.764735",
+    "tags": [
+      "sunt",
+      "labore",
+      "ut",
+      "exercitation",
+      "elit"
+    ],
+    "range": [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Randi Barrett"
+      },
+      {
+        "id": 1,
+        "name": "Hopkins Irwin"
+      },
+      {
+        "id": 2,
+        "name": "Buck Larsen"
+      }
+    ],
+    "greeting": "Hello, Ruthie! You have 6 unread messages.",
+    "favoriteFruit": "apple"
+  }
+];
